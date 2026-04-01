7dadd827f760ce006d5cc2a97f0c4489ce1044f0
Suyash Gardi <suyashgardi7119@gmail.com>
2026-04-01 19:48:10 +0530

    Fix API proxy configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-const APIURL='http://localhost:5000'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    
     proxy: {
-      '/api':{
-        target:APIURL,
-        changeOrigin:true,
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
       }
     }
   }
